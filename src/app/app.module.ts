e99f0dea1afd0a29e471d65bdbe8f0662302d0c9
Jyoti Shinde <kitto@Jyotis-MacBook-Pro.local>
2022-01-20 12:39:34 +0530

    initial setup, added employee list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule
