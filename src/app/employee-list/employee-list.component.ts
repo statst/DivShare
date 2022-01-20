4a1f168f684878c341e6ea50936fc80a001beeb8
Jyoti Shinde <kitto@Jyotis-MacBook-Pro.local>
2022-01-20 22:39:29 +0530

    added employee table

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
-  constructor() { }
+  public employeeID: string;
+  public firstName: string;
+  public lastName: string;
+  public salary: string;
+  public DOB: string;
+  public email: string;
+  public action: string;
+  
+
+  constructor() { 
+    this.employeeID = "";
+    this.firstName = "";
+    this.lastName = "";
+    this.salary = "";
+    this.DOB = "";
+    this.email = "";
+    this.action = "";
+  }
 
   ngOnInit(): void {
+
+
   }
 
 }
